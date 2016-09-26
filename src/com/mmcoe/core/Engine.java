83e64eeb9bf326efb8e9863baf29200d5279f159
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-26 23:46:25 +0530

    Dont Print File Path now

--- src/com/mmcoe/core/Engine.java
+++ src/com/mmcoe/core/Engine.java
@@ -8,7 +8,6 @@ public class Engine {
 		
 		StringFinder SF = new StringFinder();
 		for(int i=1;i<args.length;i++) {
-		System.out.println("\nFile: "+args[i]);
 		SF.findString(args[0],args[i]);
 		}
 			
