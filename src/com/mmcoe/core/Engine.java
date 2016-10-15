fb7600b32ebd886786ec31d2ee79464fbe216e1d
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-10-15 15:05:48 +0530

    source: Aesthetic Update

--- src/com/mmcoe/core/Engine.java
+++ src/com/mmcoe/core/Engine.java
@@ -12,48 +12,48 @@ import com.mmcoe.core.utils.StringFinder;
 
 public class Engine {
 
-	private static String WHY = "WHY";
-	private static String WHICH = "WHICH";
-	private static String WHAT = "WHAT";
-	private static String WHEN = "WHEN";
-	private static String HOW = "HOW";
-	private static String WHO = "WHO";
-	private static String[] QTYPE = {WHY,WHICH,WHAT,WHEN,HOW,WHO};
-	public static void main(String[] args) throws ParseException {
-		CommandLineParser parser = new BasicParser();
-		Options options = new Options();
-		options.addOption("s", true, "Search String");
-		options.addOption("f", true, "File Name");
-		options.addOption("q", true, "Question");
-		CommandLine commandLine = parser.parse(options, args);
-		String QUESTION_TYPE = getOption('q', commandLine);
-		String SEARCH_STRING = getOption('s', commandLine);
-		String[] FILE_NAME = commandLine.getArgs();
-		StringFinder SF = new StringFinder();
-		if (QUESTION_TYPE != null) {
-			QUESTION_TYPE = QUESTION_TYPE.toUpperCase();
-			if(Arrays.asList(QTYPE).contains(QUESTION_TYPE)) {
-				for (int i = 0; i < FILE_NAME.length; i++) {
-				SF.getFirstOccurance(SEARCH_STRING, FILE_NAME[i], 1);
-				}
-			}
+    private static String WHY = "WHY";
+    private static String WHICH = "WHICH";
+    private static String WHAT = "WHAT";
+    private static String WHEN = "WHEN";
+    private static String HOW = "HOW";
+    private static String WHO = "WHO";
+    private static String[] QTYPE = {WHY,WHICH,WHAT,WHEN,HOW,WHO};
+    public static void main(String[] args) throws ParseException {
+        CommandLineParser parser = new BasicParser();
+        Options options = new Options();
+        options.addOption("s", true, "Search String");
+        options.addOption("f", true, "File Name");
+        options.addOption("q", true, "Question");
+        CommandLine commandLine = parser.parse(options, args);
+        String QUESTION_TYPE = getOption('q', commandLine);
+        String SEARCH_STRING = getOption('s', commandLine);
+        String[] FILE_NAME = commandLine.getArgs();
+        StringFinder SF = new StringFinder();
+        if (QUESTION_TYPE != null) {
+            QUESTION_TYPE = QUESTION_TYPE.toUpperCase();
+            if(Arrays.asList(QTYPE).contains(QUESTION_TYPE)) {
+                for (int i = 0; i < FILE_NAME.length; i++) {
+                    SF.getFirstOccurance(SEARCH_STRING, FILE_NAME[i], 1);
+                }
+            }
 
-		} else {
-			for (int i = 1; i < FILE_NAME.length; i++) {
-				SF.findString(SEARCH_STRING, FILE_NAME[i]);
-			}
-		}
+        } else {
+            for (int i = 1; i < FILE_NAME.length; i++) {
+                SF.findString(SEARCH_STRING, FILE_NAME[i]);
+            }
+        }
 
-	}
+    }
 
-	public static String getOption(final char option,
-			final CommandLine commandLine) {
+    public static String getOption(final char option,
+                                   final CommandLine commandLine) {
 
-		if (commandLine.hasOption(option)) {
-			return commandLine.getOptionValue(option);
-		}
+        if (commandLine.hasOption(option)) {
+            return commandLine.getOptionValue(option);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
 }
--- src/com/mmcoe/core/utils/StringFinder.java
+++ src/com/mmcoe/core/utils/StringFinder.java
@@ -5,126 +5,126 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class StringFinder {
-	private File file;
-	private String SearchStr;
-	private int FirstOccuranceFlag = 0;
-	private int[] occuranceMap = new int[3];
+    private File file;
+    private String SearchStr;
+    private int FirstOccuranceFlag = 0;
+    private int[] occuranceMap = new int[3];
 
-	public StringFinder() {
-		// Constructor
-	}
+    public StringFinder() {
+        // Constructor
+    }
 
-	public void findString(String query, String filePath) {
-		file = new File(filePath);
-		SearchStr = query;
-		try {
-			Scanner S = new Scanner(file);
-			int lineNumber = 0, occurancesFound = 0, paraNumber = 1;
-			if (FirstOccuranceFlag != 0) {
-				lineNumber = occuranceMap[0];
-				occurancesFound = occuranceMap[1];
-				paraNumber = occuranceMap[2];
-			}
-			while (S.hasNextLine()) {
-				lineNumber++;
-				String line = S.nextLine();
-				if (!isBlank(line)) {
-					if (line.toLowerCase().contains(SearchStr.toLowerCase())) {
-						int occurancesFoundInLine = getAllIndexesOfSubstringInString(
-								line, SearchStr);
-						System.out.println("\"" + SearchStr
-								+ "\" Found on: Paragraph " + paraNumber
-								+ ", line: " + lineNumber + ": "
-								+ occurancesFoundInLine + " Occurances");
-						occurancesFound += occurancesFoundInLine;
+    public void findString(String query, String filePath) {
+        file = new File(filePath);
+        SearchStr = query;
+        try {
+            Scanner S = new Scanner(file);
+            int lineNumber = 0, occurancesFound = 0, paraNumber = 1;
+            if (FirstOccuranceFlag != 0) {
+                lineNumber = occuranceMap[0];
+                occurancesFound = occuranceMap[1];
+                paraNumber = occuranceMap[2];
+            }
+            while (S.hasNextLine()) {
+                lineNumber++;
+                String line = S.nextLine();
+                if (!isBlank(line)) {
+                    if (line.toLowerCase().contains(SearchStr.toLowerCase())) {
+                        int occurancesFoundInLine = getAllIndexesOfSubstringInString(
+                                                        line, SearchStr);
+                        System.out.println("\"" + SearchStr
+                                           + "\" Found on: Paragraph " + paraNumber
+                                           + ", line: " + lineNumber + ": "
+                                           + occurancesFoundInLine + " Occurances");
+                        occurancesFound += occurancesFoundInLine;
 
-					}
-				} else {
-					paraNumber++;
-				}
-			}
-			if (occurancesFound == 0) {
-				System.out.println("\"" + SearchStr + "\" Not Found");
-			} else {
-				System.out
-						.println(occurancesFound + " Total Occurances Found.");
-			}
-			S.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Scanner Cannot Scan the file: " + filePath
-					+ "\n\nLog:\n\n");
-			e.printStackTrace();
-		}
-	}
+                    }
+                } else {
+                    paraNumber++;
+                }
+            }
+            if (occurancesFound == 0) {
+                System.out.println("\"" + SearchStr + "\" Not Found");
+            } else {
+                System.out
+                .println(occurancesFound + " Total Occurances Found.");
+            }
+            S.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Scanner Cannot Scan the file: " + filePath
+                               + "\n\nLog:\n\n");
+            e.printStackTrace();
+        }
+    }
 
-	public static boolean isBlank(String str) {
-		int strLen;
-		if (str == null || (strLen = str.length()) == 0) {
-			return true;
-		}
-		for (int i = 0; i < strLen; i++) {
-			if ((Character.isWhitespace(str.charAt(i)) == false)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(str.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private static int getAllIndexesOfSubstringInString(String fullString,
-			String substring) {
-		int atIndex = 0;
-		int count = 0;
-		while (atIndex != -1) {
-			atIndex = fullString.indexOf(substring, atIndex);
+    private static int getAllIndexesOfSubstringInString(String fullString,
+            String substring) {
+        int atIndex = 0;
+        int count = 0;
+        while (atIndex != -1) {
+            atIndex = fullString.indexOf(substring, atIndex);
 
-			if (atIndex != -1) {
-				count++;
-				atIndex += substring.length();
-			}
-		}
-		return count;
-	}
+            if (atIndex != -1) {
+                count++;
+                atIndex += substring.length();
+            }
+        }
+        return count;
+    }
 
-	public void getFirstOccurance(String query, String filePath, int PrintFlag) {
-		FirstOccuranceFlag = 1;
-		file = new File(filePath);
-		SearchStr = query;
-		int lineNumber = 0, occurancesFound = 0, paraNumber = 1;
-		try {
-			Scanner S = new Scanner(file);
-			
-			while (S.hasNextLine()) {
-				lineNumber++;
-				String line = S.nextLine();
-				if (!isBlank(line)) {
-					if (line.toLowerCase().contains(SearchStr.toLowerCase())) {
-						int occurancesFoundInLine = getAllIndexesOfSubstringInString(
-								line, SearchStr);
-						occurancesFound += occurancesFoundInLine;
-						break;
-					}
-				} else {
-					paraNumber++;
-				}
-			}
-			S.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Scanner Cannot Scan the file: " + filePath
-					+ "\n\nLog:\n\n");
-			e.printStackTrace();
-		}
-		
-		occuranceMap[0] = lineNumber;
-		occuranceMap[1]=occurancesFound;
-		occuranceMap[2]=paraNumber;
-		if(PrintFlag==0){
-			return;
-		} else {
-			System.out.println("\"" + SearchStr
-					+ "\" Found on: Paragraph " + occuranceMap[2]
-					+ ", line: " + occuranceMap[0] + ": "
-					+ occuranceMap[1] + " Occurances");
-		}
+    public void getFirstOccurance(String query, String filePath, int PrintFlag) {
+        FirstOccuranceFlag = 1;
+        file = new File(filePath);
+        SearchStr = query;
+        int lineNumber = 0, occurancesFound = 0, paraNumber = 1;
+        try {
+            Scanner S = new Scanner(file);
 
-	}
+            while (S.hasNextLine()) {
+                lineNumber++;
+                String line = S.nextLine();
+                if (!isBlank(line)) {
+                    if (line.toLowerCase().contains(SearchStr.toLowerCase())) {
+                        int occurancesFoundInLine = getAllIndexesOfSubstringInString(
+                                                        line, SearchStr);
+                        occurancesFound += occurancesFoundInLine;
+                        break;
+                    }
+                } else {
+                    paraNumber++;
+                }
+            }
+            S.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Scanner Cannot Scan the file: " + filePath
+                               + "\n\nLog:\n\n");
+            e.printStackTrace();
+        }
+
+        occuranceMap[0] = lineNumber;
+        occuranceMap[1]=occurancesFound;
+        occuranceMap[2]=paraNumber;
+        if(PrintFlag==0) {
+            return;
+        } else {
+            System.out.println("\"" + SearchStr
+                               + "\" Found on: Paragraph " + occuranceMap[2]
+                               + ", line: " + occuranceMap[0] + ": "
+                               + occuranceMap[1] + " Occurances");
+        }
+
+    }
 }
